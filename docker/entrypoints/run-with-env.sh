057b5743dec3ca56ddaf524d9fdffe8bff14c7b3
mu wang <wm140906@gmail.com>
2026-03-20 12:57:53 -0400

    fix: load docker env files literally

--- docker/entrypoints/run-with-env.sh
+++ docker/entrypoints/run-with-env.sh
@@ -2,9 +2,23 @@
 set -eu
 
 if [ -f /app/.env ]; then
-  set -a
-  . /app/.env
-  set +a
+  while IFS= read -r line || [ -n "$line" ]; do
+    case "$line" in
+      ''|'#'*)
+        continue
+        ;;
+      export\ *)
+        line=${line#export }
+        ;;
+    esac
+
+    key=${line%%=*}
+    value=${line#*=}
+
+    if [ -n "$key" ] && [ "$key" != "$line" ]; then
+      export "$key=$value"
+    fi
+  done < /app/.env
 fi
 
 export DATABASE_URL="${DATABASE_URL:-postgres://agent_roster:agent_roster@postgres:5432/agent_roster}"
